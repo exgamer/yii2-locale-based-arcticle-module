885d2287f53ca473f5eb9b66ba32521cb218fdc7
Olzhas Kulzhambekov <exgamer@live.ru>
2020-02-15 19:36:25 +0600

    +

--- src/services/PostService.php
+++ src/services/PostService.php
@@ -1,6 +1,7 @@
 <?php
 namespace concepture\yii2article\services;
 
+use concepture\yii2handbook\traits\ServicesTrait;
 use concepture\yii2logic\services\traits\ViewsTrait;
 use yii\db\ActiveQuery;
 use concepture\yii2article\traits\ServicesTrait as ArticleServices;
@@ -25,6 +26,7 @@ use concepture\yii2handbook\services\traits\EntityTypeSupportTrait;
 class PostService extends Service
 {
     use ArticleServices;
+    use ServicesTrait;
     use StatusTrait;
     use PropertyTrait;
     use HandbookModifySupportTrait;
@@ -41,5 +43,47 @@ class PostService extends Service
     protected function afterModelSave(Model $form , ActiveRecord $model, $is_new_record)
     {
         $this->postCategoryService()->updatePostCount($form->category_id);
+    }
+
+    public function getDataProviderWithPosition($page = 0, $perPage = 50, $entity_type_position = null)
+    {
+        $config = [
+            'pagination' => [
+                'pageSize' => $perPage,
+                'pageSizeParam' => false,
+                'forcePageParam' => false,
+            ]
+        ];
+        if(! $entity_type_position) {
+            $config['sort'] = [
+                'attributes' => ['created_at'],
+                'defaultOrder' => [
+                    'created_at' => SORT_DESC,
+                ]
+            ];
+        }
+
+        return $this->getDataProvider([], $config, null, null,  function(ActiveQuery $query) use ($entity_type_position){
+            if ($entity_type_position) {
+                $entityTableName = $this->getTableName();
+                $entity_type = $this->entityTypeService()->getOneByCondition(['table_name' => $entityTableName], true);
+                if (!$entity_type) {
+                    throw new Exception('Entity type not found.');
+                }
+
+                $this->entityTypePositionSortService()->applyQuery(
+                    $query,
+                    $entityTableName,
+                    $entity_type->id,
+                    $entity_type_position,
+                    [
+                        'created_at' => SORT_DESC
+                    ]
+                );
+            }
+
+            $query->andWhere([Match::tableName() . '.status' => StatusEnum::ACTIVE]);
+            $query->andWhere([Match::tableName() . '.is_deleted' => IsDeletedEnum::NOT_DELETED]);
+        });
     }
 }
